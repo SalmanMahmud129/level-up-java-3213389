24499d73183376dad4f09b902a44c9842125d170
Salman Mahmud <salman_mahmud@hotmail.com>
2023-07-25 18:17:14 -0400

    isPasswordComplex

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,29 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        int upperCase = 0;
+        int lowerCase = 0;
+        int length = 0;
+        int number = 0;
+
+        // char[] ch = password.toCharArray(); 
+        
+
+        for(int i = 0; i < password.length(); i++ ){
+            length++;
+            char ch = password.charAt(i);
+            if(Character.isUpperCase(ch)){
+                upperCase++;
+            }else if (Character.isLowerCase(ch)){
+                lowerCase++;
+            }else if(Character.isDigit(ch)){
+                number++;
+            }
+
+        }
+
+        return upperCase > 0 && lowerCase > 0 && length >= 6 && number > 0;
+        
     }
 
     public static void main(String[] args) {
