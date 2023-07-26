ef31c1e71dfcf48bcef5077b9dd112dbf6bccec3
Salman Mahmud <salman_mahmud@hotmail.com>
2023-07-25 20:09:43 -0400

    water bill

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -6,7 +6,26 @@ public class App {
 
     public static double calculateWaterBill(double gallonsUsage) {
         // TODO: Implement method
+        double minimumBill = 18.84;
+        double gallonsPerCCF = 748;
+        double includedGallons = 2 * gallonsPerCCF;
+
+        if (gallonsUsage <= includedGallons) {
+            return minimumBill;
+        } else if (gallonsUsage > includedGallons) {
+            // System.out.println(gallonsUsage - includedGallons);
+            if (gallonsUsage - includedGallons <= gallonsPerCCF) {
+                System.out.println("Test");
+                return minimumBill + 3.90;
+            } else {
+                double numberOfExtraCCFs = (gallonsUsage - includedGallons) / gallonsPerCCF;
+                // System.out.println("Extras " + numberOfExtraCCFs);
+                return minimumBill + (Math.ceil(numberOfExtraCCFs) * 3.90);
+            }
+        }
+
         return 0;
+
     }
 
     public static void main(String[] args) {
